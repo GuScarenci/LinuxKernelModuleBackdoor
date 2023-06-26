8395eb73744d648dd251a5619ecd79b44e96b10b
gabriel-libardi <libardi.120205@protonmail.com>
2023-06-26 11:37:38 -0300

    Tried to fix wrong format

---
+++ src/code.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/***************Funções de Módulo****************/
+
+MODULE_LICENSE("GPL");
+
+static int segmentsDisplay_init(void)
+{
+    printk(KERN_ALERT "MÓDULO INICIALIZADO!");
+    return 0;
+}
+
+static void segmentsDisplay_exit(void)
+{
+    printk(KERN_ALERT "MÓDULO REMOVIDO!");
+}
+
+module_init(segmentsDisplay_init);
+module_exit(segmentsDisplay_exit);
