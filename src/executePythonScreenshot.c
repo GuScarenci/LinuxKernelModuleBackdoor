d6eac6a4be2106443f9e99bfdb8315af8d0a30f1
Gustavo Scarenci <gustavomourascarenci@usp.br>
2023-06-28 09:18:34 -0300

    a

---
+++ src/executePythonScreenshot.c
@@ -0,0 +1,35 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kmod.h>
+
+static int __init execute_python_init(void)
+{
+    char *argv[] = { "/usr/bin/python", "/path/to/your/python/script.py", NULL };
+    static char *envp[] = {
+        "HOME=/",
+        "TERM=linux",
+        "PATH=/sbin:/usr/sbin:/bin:/usr/bin",
+        NULL
+    };
+    
+    printk(KERN_INFO "Executing Python script...\n");
+    
+    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+    
+    printk(KERN_INFO "Python script executed.\n");
+    
+    return 0;
+}
+
+static void __exit execute_python_exit(void)
+{
+    printk(KERN_INFO "Exiting module.\n");
+}
+
+module_init(execute_python_init);
+module_exit(execute_python_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("Kernel module to execute a Python script");
